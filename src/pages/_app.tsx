a598d5e82ffc2b679642663ff65e04565ba6f46a
Voloshin Sergei <4voloshin@gmail.com>
2023-04-20 17:01:29 +0300

    feat: #6 added sign up page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import '@/styles/globals.css'
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: 'SF Compact Display, sans-serif',
+  },
+  palette: {
+    primary: {
+      main: '#1da1f2',
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
