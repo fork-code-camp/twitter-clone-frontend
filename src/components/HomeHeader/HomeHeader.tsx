36e3136f316531e30d4b27a0e10567aa82833a9c
NaN-Simon <goosaimon@gmail.com>
2023-04-23 20:19:03 +0300

    feat: add HomeHeader component

---
+++ src/components/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, IconButton, Typography } from '@mui/material';
+import TopTweet from '@/assets/TopTweet.svg';
+
+const onClick = () => {
+  console.log('HomeHeader click');
+};
+
+const HomeHeader = () => {
+  return (
+    <Container
+      disableGutters
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        gap: '0 13px',
+        maxWidth: { xs: '320px', md: '600px' },
+        padding: '10px 15px',
+        border: '1px solid #ebeef0',
+      }}
+    >
+      <Typography
+        fontWeight={700}
+        fontSize={19}
+        lineHeight="23px"
+        color="#0F1419"
+      >
+        Home
+      </Typography>
+      <IconButton sx={{ m: 0, p: 0 }} onClick={() => onClick()}>
+        <TopTweet style={{ fill: '#1DA1F2' }} />
+      </IconButton>
+    </Container>
+  );
+};
+
+export default HomeHeader;
