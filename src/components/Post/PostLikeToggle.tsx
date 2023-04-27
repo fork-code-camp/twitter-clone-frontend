a34e243842a5e081ae7670affbce6d31d8b916e0
NaN-Simon <goosaimon@gmail.com>
2023-04-27 22:17:38 +0300

    feat: add PostLikeToggle

---
+++ src/components/Post/PostLikeToggle.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useState, useEffect } from 'react';
+import { Box, ToggleButton, Typography } from '@mui/material';
+import LikeSVG from '@/assets/Like.svg';
+
+type ILikeToggle = {
+  count: number;
+  selected?: boolean;
+  onChangeLike: (likeCount: number) => void;
+};
+
+const notSelectedColor = '#5B7083';
+const selectedColor = '#F4245E';
+
+const LikeToggle: FC<ILikeToggle> = ({ count, selected, onChangeLike }) => {
+  const [selectedBtn, setSelectedBtn] = useState(selected);
+  const [likeCount, setLikeCount] = useState(count);
+
+  useEffect(() => onChangeLike(likeCount), [likeCount, onChangeLike]);
+
+  return (
+    <ToggleButton
+      value="check"
+      selected={selectedBtn}
+      onChange={() => {
+        setSelectedBtn((selected) => !selected);
+        setLikeCount((likeCount) =>
+          selectedBtn ? likeCount - 1 : likeCount + 1
+        );
+      }}
+      sx={{ display: 'flex', alignItems: 'center', gap: 1.25, p: 0 }}
+    >
+      <Box sx={{display: 'flex', strokeWidth: 2 }}>
+        <LikeSVG
+          style={{
+            stroke: selectedBtn ? selectedColor : notSelectedColor,
+            fill: selectedBtn && selectedColor,
+          }}
+        />
+      </Box>
+      <Typography
+        component="span"
+        sx={{ color: notSelectedColor, lineHeight: 1.1 }}
+      >
+        {likeCount}
+      </Typography>
+    </ToggleButton>
+  );
+};
+
+export default LikeToggle;
