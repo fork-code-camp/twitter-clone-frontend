671771199adf19ee16630713d78b08311f5ff578
NaN-Simon <goosaimon@gmail.com>
2023-10-04 10:17:54 +0300

    refactor: disable bootmenu page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,98 +1,24 @@
 import React from 'react';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo.config';
-import { Button, Container, Link, Box, useTheme } from '@mui/material';
-import { useLogoutQuery } from '@/query/authorization/authorization.query';
-const Home = () => {
-  const { refetch: logout } = useLogoutQuery();
+import HomeView from '@/views/home/HomeView';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Container } from '@mui/material';
+import { check } from '../api/authCheck';
 
-  const theme = useTheme();
-  const btnTheme = {
-    fontFamily: theme.typography.button.fontFamily,
-    fontStyle: theme.typography.button.fontStyle,
-    fontWeight: theme.typography.button.fontWeight,
-    fontSize: theme.typography.button.fontSize,
-    lineHeight: theme.typography.button.lineHeight,
-    color: theme.typography.button.color,
-  };
+const Home = () => {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    check().then((res) => !res && push('/logout'));
+  }, [push]);
+
   return (
     <>
-      <DefaultSeo title="Boot Menu" {...SEO} />
-      <Container
-        disableGutters
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          gap: 1,
-          p: 1,
-        }}
-      >
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-          <Button
-            component={Link}
-            href="/signUp"
-            variant="contained"
-            sx={btnTheme}
-          >
-            SignUp page
-          </Button>
-          <Button
-            component={Link}
-            href="/signIn"
-            variant="contained"
-            sx={btnTheme}
-          >
-            Login page
-          </Button>
-          <Button
-            component={Link}
-            href="/logout"
-            variant="contained"
-            sx={btnTheme}
-          >
-            Logout(View) page
-          </Button>
-        </Box>
-
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-          <Button
-            component={Link}
-            href="/home"
-            variant="contained"
-            sx={btnTheme}
-          >
-            Home page
-          </Button>
-          <Button
-            component={Link}
-            href="/explore"
-            variant="contained"
-            sx={btnTheme}
-          >
-            Explore page
-          </Button>
-          <Button
-            component={Link}
-            href="/profile"
-            variant="contained"
-            sx={btnTheme}
-          >
-            Profile page
-          </Button>
-          <Button
-            component={Link}
-            href="/404"
-            variant="contained"
-            sx={btnTheme}
-          >
-            404 page
-          </Button>
-        </Box>
-
-        <Button onClick={() => logout()} variant="contained" sx={btnTheme}>
-          Logout Auth
-        </Button>
+      <DefaultSeo title="Home" {...SEO} />
+      <Container>
+        <HomeView />
       </Container>
     </>
   );
